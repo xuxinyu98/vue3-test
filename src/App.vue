85f0eb91d7b26b7c275cde6fc8027a2a432bdb4b
xuxinyu <2313272975@qq.com>
2022-06-30 14:53:56 +0800

    master

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>INS管理后台</h1>
+  <h3>用户名</h3>
+  <input>
+  <h3>密码</h3>
+
+</template>
+
+<script>
+
+  export default{
+    name:'APP',
+    setup(){
+      
+    }
+  }
+</script>
+
